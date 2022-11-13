274343e2cd09ff5e798a7942562697907cfd4877
vladyslav_f@optimove.com <vladyslav_f@optimove.com>
2022-11-13 17:31:05 +0100

    changes

--- public_html/catalog/controller/revolution/revpopupcartquick.php
+++ public_html/catalog/controller/revolution/revpopupcartquick.php
@@ -538,7 +538,7 @@ class ControllerRevolutionRevpopupcartQuick extends Controller {
 
 // запрос
         $curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, 'https://bilioscrm.com.ua/engine/api/addorder.php');
+		curl_setopt($curl, CURLOPT_URL, 'https://dev.bilios.com.ua/engine/api/addorder.php');
         //scurl_setopt($curl, CURLOPT_URL, 'https://webhook.site/e1c94e83-f99b-4e03-a626-68a541dd4ef4');
         curl_setopt($curl, CURLOPT_POST, true);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
