93aa312d669e7459fb41bd2a1d2f3abb42e3fe11
vladyslav_f@optimove.com <vladyslav_f@optimove.com>
2022-11-18 13:14:11 +0100

    asdfasdfadsf

--- public_html/index.php
+++ public_html/index.php
@@ -7,12 +7,12 @@ error_reporting(0);
 
 $period_cookie = 2592000; // 30 дней (2592000 секунд)
 
-if($_GET){
-    setcookie("utm_source",$_GET['utm_source'],time()+$period_cookie);
-    setcookie("utm_medium",$_GET['utm_medium'],time()+$period_cookie);
-    setcookie("utm_term",$_GET['utm_term'],time()+$period_cookie);
-    setcookie("utm_content",$_GET['utm_content'],time()+$period_cookie);
-    setcookie("utm_campaign",$_GET['utm_campaign'],time()+$period_cookie);
+if($_GET['utm_source'] || $_GET['utm_medium'] || $_GET['utm_term'] || $_GET['utm_content'] || $_GET['utm_campaign']){
+	setcookie("utm_source",($_GET['utm_source']? $_GET['utm_source']: '' ), time() +$period_cookie);
+	setcookie("utm_medium",($_GET['utm_medium']? $_GET['utm_medium']: '' ), time() +$period_cookie);
+	setcookie("utm_term",($_GET['utm_term']? $_GET['utm_term']: '' ), time() +$period_cookie);
+	setcookie("utm_content",($_GET['utm_content']? $_GET['utm_content']: '' ), time() +$period_cookie);
+	setcookie("utm_campaign", ($_GET['utm_campaign']? $_GET['utm_campaign']: ''), time() +$period_cookie);
 }
 
 // Configuration
