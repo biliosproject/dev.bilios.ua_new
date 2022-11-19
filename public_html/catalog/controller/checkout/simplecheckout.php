b5b74e68381d5a907ba3c64ef89b51fe540d7a55
Igor Fedun <biliosproject@gmail.com>
2022-11-19 13:06:28 +0200

    multiple items in one request simplecheckout.php

--- public_html/catalog/controller/checkout/simplecheckout.php
+++ public_html/catalog/controller/checkout/simplecheckout.php
@@ -1330,7 +1330,7 @@ class ControllerCheckoutSimpleCheckout extends SimpleController {
         $utm_config_dir_path = $current_dir_path . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . ".." . DIRECTORY_SEPARATOR . "..";
         $utm_config_file = $utm_config_dir_path . DIRECTORY_SEPARATOR . "utm_config.php";
         include($utm_config_file);
-
+        $products_list = [];
         foreach ($all_products as &$product) {
 
             $product_model = "";
@@ -1342,47 +1342,45 @@ class ControllerCheckoutSimpleCheckout extends SimpleController {
             if (!empty($product_model)) {
                 $product_id = $product_model;
             }
-            $products_list = array(
-                0 => array(
+            $single_product_list = array(
                     'product_id' => $product_id,    //код товара (из каталога CRM)
                     'price' => $product["price"], //цена товара 1
                     'count' => $product["quantity"],                     //количество товара 1
                     // если есть смежные товары, тогда количество общего товара игнорируется
-                )
             );
-
-            $products = urlencode(serialize($products_list));
-            $sender = urlencode(serialize($_SERVER));
-            // параметры запроса
-            $data_for_request = array(
-                'key' => $crm_api_key, //Ваш секретный токен
-                'products' => $products,                    // массив с товарами в заказе
-                'bayer_name' => $data["firstname"],  // покупатель (Ф.И.О)
-                'phone' => $data["telephone"],  // телефон
-                'email' => $data["email"], // электронка
-                'comment' => 'Звичайне замовлення',  // комментарий
-                'delivery' => '',  // способ доставки (id в CRM)
-                'delivery_adress' => '',  // адрес доставки
-                'payment' => '',  // вариант оплаты (id в CRM)
-                'sender' => $sender,
-                'utm_source'      => $_COOKIE['utm_source'],  // utm_source
-                'utm_medium'      => $_COOKIE['utm_medium'],  // utm_medium
-                'utm_term'        => $_COOKIE['utm_term'],  // utm_term
-                'utm_content'     => $_COOKIE['utm_content'],  // utm_content
-                'utm_campaign'    => $_COOKIE['utm_campaign'],  // utm_campaign
-            );
-
-            // запрос
-            $curl = curl_init();
-            curl_setopt($curl, CURLOPT_URL, 'https://dev.bilios.com.ua/engine/api/addorder.php');
-            // curl_setopt($curl, CURLOPT_URL, 'https://bilioscrm.com.ua/engine/api/addorder.php');
-            //curl_setopt($curl, CURLOPT_URL, 'https://webhook.site/e1c94e83-f99b-4e03-a626-68a541dd4ef4');
-            curl_setopt($curl, CURLOPT_POST, true);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $data_for_request);
-            $out = curl_exec($curl);
-            curl_close($curl);
+            array_push($products_list, $single_product_list);
         }
+        $products = urlencode(serialize($products_list));
+        $sender = urlencode(serialize($_SERVER));
+        // параметры запроса
+        $data_for_request = array(
+            'key' => $crm_api_key, //Ваш секретный токен
+            'products' => $products,                    // массив с товарами в заказе
+            'bayer_name' => $data["firstname"],  // покупатель (Ф.И.О)
+            'phone' => $data["telephone"],  // телефон
+            'email' => $data["email"], // электронка
+            'comment' => 'Звичайне замовлення',  // комментарий
+            'delivery' => '',  // способ доставки (id в CRM)
+            'delivery_adress' => '',  // адрес доставки
+            'payment' => '',  // вариант оплаты (id в CRM)
+            'sender' => $sender,
+            'utm_source'      => $_COOKIE['utm_source'],  // utm_source
+            'utm_medium'      => $_COOKIE['utm_medium'],  // utm_medium
+            'utm_term'        => $_COOKIE['utm_term'],  // utm_term
+            'utm_content'     => $_COOKIE['utm_content'],  // utm_content
+            'utm_campaign'    => $_COOKIE['utm_campaign'],  // utm_campaign
+        );
+
+        // запрос
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://dev.bilios.com.ua/engine/api/addorder.php');
+        // curl_setopt($curl, CURLOPT_URL, 'https://bilioscrm.com.ua/engine/api/addorder.php');
+        //curl_setopt($curl, CURLOPT_URL, 'https://webhook.site/e1c94e83-f99b-4e03-a626-68a541dd4ef4');
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_for_request);
+        $out = curl_exec($curl);
+        curl_close($curl);
         return $order_id;
     }
 
